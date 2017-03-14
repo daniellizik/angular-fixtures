1dd882ca6377b39b0f5a76e2112117073241e433
daniellizik <daniellizik@gmail.com>
2017-03-14 10:18:24 -0400

    added index html

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const { readdirSync, readFileSync } = require('fs')
 const path = require('path')
 const dist = './dist'
+const www = './www'
+
+const html = readdirSync(www)
+  .reduce((files, f) => {
+    files[f.split('.').shift()] = readFileSync(`${www}/${f}`, 'utf8')
+    return files
+  }, {})
 
 const data = readdirSync(dist)
   .reduce((version, dir) => {
     version[dir] = readdirSync(`${dist}/${dir}`).reduce((files, f) => {
       const file = `${dist}/${dir}/${f}`
-      files[f.split('.').shift()] = {
+      const filename = f.split('.').shift()
+      files[filename] = {
         require: () => require(file),
-        string: readFileSync(file, 'utf8')
+        string: readFileSync(file, 'utf8'),
+        html: html[filename]
       }
       return files
     }, {})
